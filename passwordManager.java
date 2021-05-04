68cbaa509f1191a5ce048799cbe6597c53a4933e
dWilletts <43909902+DonaldWilletts@users.noreply.github.com>
2021-05-04 17:59:12 -0500

    Create passwordManager.java
    Created passwordManager file

---
+++ passwordManager.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.util.*;
+import java.io.File ; 
+import java.nio.*; 
+
+public class passwordManager
+
+{
+    public static void main(String args[])
+    {
+        if(args.length != 2)
+        {
+            System.out.println("Test");
+            return ; 
+        }
+        
+    }
+}
