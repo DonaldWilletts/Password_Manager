9d420ca72255c6d52c85150038a40dc3158c5ba8
dWilletts <43909902+DonaldWilletts@users.noreply.github.com>
2021-05-07 17:48:16 -0500

    Need UI
    Just need to add UI all core elements are here

--- passwordManager.java
+++ passwordManager.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.OutputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.File ; 
 import java.nio.file.Files;
 import java.nio.*; 
@@ -11,8 +12,13 @@ import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.util.concurrent.TimeUnit;
+
 import java.nio.file.Path;
 
 //public static final String masterFilePath = "C:\\Users\\donwi\\Documents\\GitHub\\Password_Manager\\masterFile.txt";
@@ -21,21 +27,63 @@ public class passwordManager
 {
     public static void main(String args[])
     {
-        
-        //ArrayList<String> inputData = new ArrayList<String>(); 
-        //inputData.add("Account Name");
-        //inputData.add("Username");
-        //inputData.add("Password");
-        //masterFileClass.writeToMasterFile(inputData);
-        
+        try{
+            /*
+            System.out.println("Creating Secure Password...");
+            TimeUnit.SECONDS.sleep(1);
+            String myPassword = passwordClass.createSecurePassword();
+            System.out.println("Your password is: " + myPassword );
 
-        //ArrayList<String> myData = masterFileClass.readFromMasterFile();
+            System.out.println("Generating secure IV and Key from password...");
+            TimeUnit.SECONDS.sleep(1);
+            byte[] key = passwordClass.getKEYFromPassword(myPassword);
+            byte[] iv = passwordClass.getIVFromPassword(myPassword); 
+            System.out.println("Success!");
+            
+            Scanner userInput = new Scanner(System.in); 
+            System.out.println("Enter Account Name: ");
+            String accountName = userInput.nextLine();
+            System.out.println("Enter Account Username: ");
+            String username = userInput.nextLine();
+            System.out.println("Enter Account Password: ");
+            String password = userInput.nextLine();
+            userInput.close();
 
-        //System.out.println(myData.toString());
+            ArrayList<String> arrayToSendToFile = new ArrayList<String>();
+            arrayToSendToFile.add(accountName); 
+            arrayToSendToFile.add(username); 
+            arrayToSendToFile.add(password);
+            masterFileClass.writeToMasterFile(arrayToSendToFile); 
+            TimeUnit.SECONDS.sleep(7);
 
-        //encrytionClass.setupAndEncrypt(args[0], args[1], args[2], args[3]);
+            System.out.println("Encrypting masterFile...");
+            TimeUnit.SECONDS.sleep(1);
+            //create master file here? 
+            File masterFile = new File("C:\\Users\\donwi\\Documents\\GitHub\\Password_Manager\\masterFile.txt");
+            encrytionClass.setupAndEncrypt("masterFile", iv, key);
+            System.out.println("masterFile successfully encrypted!");
+            TimeUnit.SECONDS.sleep(7);
 
-        //decryptionClass.setupAndDecrypt(args[0], args[1], args[2], args[3]);
+            System.out.println("Decrypting masterFile...");
+            TimeUnit.SECONDS.sleep(1);
+            decryptionClass.setupAndDecrypt(iv, key); 
+            System.out.println("masterFile successfully decrypted!");
+            */
+
+            Scanner userSearch = new Scanner(System.in); 
+            System.out.println("Enter Account Name to search for: ");
+            String accountNameToQuery = userSearch.nextLine();
+            ArrayList<String> returnList = masterFileClass.getAccountNameUsernamePasswordFromMasterFile(accountNameToQuery);
+            System.out.println("Username and Password: " + returnList.toString());
+
+            userSearch.close();
+            
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
 
         return ; 
         
@@ -43,6 +91,11 @@ public class passwordManager
 }
 
 class masterFileClass{
+
+    public static File getMasterFile(){
+        File masterFile = new File("C:\\Users\\donwi\\Documents\\GitHub\\Password_Manager\\masterFile.txt");
+        return (masterFile);
+    }
 
     //Checking if password masterFile is in directory 
     public static boolean checkIfMasterFileExists(){
@@ -69,9 +122,9 @@ class masterFileClass{
 
     //write to masterFile | dataToWrite is List where [accountName, username, password]
     public static void writeToMasterFile(ArrayList<String> dataToWrite){
-        //should have already tested if masterFile is avaiable before being called. 
+        //should have already tested if masterFile is avaiable before being called.
         try{
-            FileWriter masterFile = new FileWriter("masterFile.txt");
+            FileWriter masterFile = new FileWriter("masterFile.txt", true);
             
             for (int i = 0; i < dataToWrite.size(); i++){
                 masterFile.write(dataToWrite.get(i) + System.lineSeparator());
@@ -109,34 +162,43 @@ class masterFileClass{
         
     }
 
+    public static Boolean existisInMasterFile(String searchString){
+        ArrayList<String> masterFileAsArrayList = readFromMasterFile();
+
+        return(masterFileAsArrayList.contains(searchString));
+    }
+
+    public static ArrayList<String> getAccountNameUsernamePasswordFromMasterFile(String accountName){
+        ArrayList<String> masterFileAsArrayList = readFromMasterFile();
+        int accountIndex = masterFileAsArrayList.indexOf(accountName);
+
+        String accountName2 = masterFileAsArrayList.get(accountIndex);
+        String username = masterFileAsArrayList.get(accountIndex+1);
+        String password = masterFileAsArrayList.get(accountIndex+2);
+
+        ArrayList<String> returnListOfQueriedData = new ArrayList<String>(); 
+        returnListOfQueriedData.add(accountName2);
+        returnListOfQueriedData.add(username);
+        returnListOfQueriedData.add(password);
+
+        return returnListOfQueriedData; 
+
+    }
 }
 
 class encrytionClass{
     //citation https://www.novixys.com/blog/java-aes-example/ || I am copying my code from HW3 here. This is the website I cited there as well. 
     
-    public static void setupAndEncrypt(String inputPlaintextFile, String outputKeyFile, String outputIVFile, String outputCiphertextFile)
+    public static void setupAndEncrypt(String masterFile, byte[] iv, byte[] key)
     {
        
         try{
-
-            // Generate Raw IV Data using IVGen function 
-            byte[] iv = IVGen(); 
-
-            // Write Raw IV data to the file name provided by outputIVFile  
-            writeByteToFile(iv, outputIVFile);
 
             // Generate IvParameterSpec object from IV Raw Data 
             IvParameterSpec ivspec = new IvParameterSpec(iv);
 
             // Generate AES Key through KeyGenerator and SecretKey (handle exception with try-catch)
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            SecretKey skey = kgen.generateKey();
-
-            // Obtain raw key bytes from the SecretKey instance 
-            byte[] keyb = skey.getEncoded(); 
-
-            //Write raw key bytes into the filed name provided by outputKeyFile 
-            writeByteToFile(keyb, outputKeyFile);
+            SecretKey skey = new SecretKeySpec(key, "AES");
 
             //Create a Cipher object with "AES/CBC/PKCS5Padding" (handle exception with try-catch)
             Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -145,7 +207,7 @@ class encrytionClass{
             ci.init(Cipher.ENCRYPT_MODE, skey, ivspec);
 
             //Call the doEncryption function to actually encrypt the file and output the ciphertext in the output file 
-            doEncryptionAndWriteToFile(ci, inputPlaintextFile);
+            doEncryptionAndWriteToFile(ci);
 
         }
         catch (Exception catchedExeption)
@@ -155,37 +217,7 @@ class encrytionClass{
 
     }
 
-    public static byte[] IVGen()
-    { 
-        try{
-            SecureRandom sRandom = SecureRandom.getInstance("SHA1PRNG","SUN");
-        
-            byte[] iv = new byte[128/8];
-            sRandom.nextBytes(iv);
-
-            return (iv);
-        }
-        catch (Exception catchedExeption)
-        {
-            catchedExeption.printStackTrace();
-        }
-        return (null); 
-    }
-
-    public static void writeByteToFile(byte[] data, String outputFileName)
-    {
-        FileOutputStream outputFile = null;
-        try {
-            outputFile = new FileOutputStream(outputFileName);
-            outputFile.write(data);
-            System.out.println("Wrote " + data.length + "bytes in the file: " + outputFileName); 
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("ERROR: Couldn't write to file: " + outputFileName);
-        }
-    }
-
-    public static void doEncryptionAndWriteToFile(Cipher cipherContext, String inputPlaintextFile)
+    public static void doEncryptionAndWriteToFile(Cipher cipherContext)
     {
         try{
             
@@ -244,7 +276,7 @@ class decryptionClass{
 
     }
     
-    public static void doDecryptionAndWriteToFile(Cipher cipherContext, String outputPlaintextFile, String inputCiphertextFile)
+    public static void doDecryptionAndWriteToFile(Cipher cipherContext)
     {
         //citation https://www.novixys.com/blog/java-aes-example/ || I am copying my code from HW3 here. This is the website I cited there as well. 
         try{
@@ -268,22 +300,16 @@ class decryptionClass{
         }
     }
 
-    public static void setupAndDecrypt(String outputPlaintextFile, String inputKeyFile, String inputIVFile, String inputCiphertextFile)
+    public static void setupAndDecrypt(byte[] iv, byte[] key)
     {
         
         try
         {   
-            // load raw IV bytes from the file given by inputIVFile (Use readBytesFromFile) IV LENGTH = 16 BYTES
-            byte[] rawIV = readByteFromFile(inputIVFile,16);
-
             // Generate IvParameterSpec object from raw IV bytes  
-            IvParameterSpec ivSpec = new IvParameterSpec(rawIV);
-
-            // load raw key bytes from the file given by inputKeyFile (Use readBytesFromFile) KEY LENGTH = 16 bytes 
-            byte[] rawKey = readByteFromFile(inputKeyFile,16);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
 
             //create a secret key spec object from raw key bytes and for "AES" algorithm (handle exception with try-catch)
-            SecretKeySpec skey = new SecretKeySpec(rawKey, "AES");
+            SecretKeySpec skey = new SecretKeySpec(key, "AES");
             
             //Create a Cipher object with "AES/CBC/PKCS5Padding" (handle exception with try-catch)
             Cipher ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -292,7 +318,7 @@ class decryptionClass{
             ci.init(Cipher.DECRYPT_MODE, skey, ivSpec);
 
             //Call the doEncryption function
-            doDecryptionAndWriteToFile(ci, outputPlaintextFile, inputCiphertextFile);
+            doDecryptionAndWriteToFile(ci);
         }
         catch(Exception e)
         {
@@ -303,6 +329,7 @@ class decryptionClass{
 }
 
 class passwordClass{
+    
     public static String createSecurePassword(){
 
         //Ciataion I referenced this code while creating my password creater: https://mkyong.com/java/java-password-generator-example/ 
@@ -313,11 +340,32 @@ class passwordClass{
         final String CHAR_UPPERCASE = CHAR_LOWERCASE.toUpperCase();
         final String DIGIT = "0123456789";
         final String SPECIAL_SYMBOLS = "!@#&()–[{}]:;',?/*~$^+=<>";
+        final String ALL_CHARACTERS = CHAR_LOWERCASE + CHAR_UPPERCASE + DIGIT + SPECIAL_SYMBOLS;
 
         //Using this to append characters for my password gen @ the end I will shuffle this to make it random
         StringBuilder password = new StringBuilder(8);
         
-        //String twoLowerCaseChars = generateRandom
+        //get 2 lowercase letters
+        password.append(getRandomCharacters(CHAR_LOWERCASE, 2));
+
+        //get 2 uppercase letters
+        password.append(getRandomCharacters(CHAR_UPPERCASE, 2));
+
+        //get 2 digits
+        password.append(getRandomCharacters(DIGIT, 2));
+
+        //get 2 special symbols 
+        password.append(getRandomCharacters(SPECIAL_SYMBOLS, 2));
+
+        //get 2 random
+        password.append(getRandomCharacters(ALL_CHARACTERS, 2));
+
+        //shuffle characters
+        List<String> shufflePassword = Arrays.asList(password.toString().split(""));
+        Collections.shuffle(shufflePassword);
+        String finalPassword = shufflePassword.stream().collect(Collectors.joining());
+
+        return (finalPassword);
 
     }
 
@@ -326,23 +374,79 @@ class passwordClass{
             //Code sourced from https://howtodoinjava.com/java8/secure-random-number-generation/ 
             SecureRandom secureRandomGen = SecureRandom.getInstance("SHA1PRNG","SUN");
             
-            //StringBuilder returnStr = new StringBuilder(amountToSendBack);
-            String returnStr = new St; 
+            StringBuilder returnStr = new StringBuilder(amountToSendBack);
 
             for (int i = 0; i < amountToSendBack; i++){
                 //get random int between 0 and the length of the strong 
                 int randomInteger = secureRandomGen.nextInt(inputString.length()-1);
 
-                returnStr = returnStr + inputString.substring(randomInteger);
+                returnStr.append(inputString.charAt(randomInteger));
 
             }
 
+            return (returnStr.toString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return (null);
+
+    }
+
+    public static byte[] getKEYFromPassword(String password){
+        try{
+            //citation: https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+            //citation: https://crypto.stackexchange.com/questions/41436/is-deriving-the-iv-from-the-password-secure
             
+            //salt that I am using is going to be the first 16 bytes of the password (only require of a salt is to be globally unique)
+            byte[] salt = Arrays.copyOfRange(password.getBytes(), 0, 16);
+            
+            PBEKeySpec passwordPBKspec = new PBEKeySpec(password.toCharArray(), salt, 1024, 256);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] createdPBEKS = skf.generateSecret(passwordPBKspec).getEncoded();
+
+            //first 16 bytes of PBEKS
+            byte[] key = Arrays.copyOfRange(createdPBEKS, 0, 16);
+
+            //System.out.println(key.length);
+
+            return key; 
 
         }
         catch (Exception e){
             e.printStackTrace();
         }
+
+        return null; 
+
     }
 
+    public static byte[] getIVFromPassword(String password){
+        try{
+            //citation: https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+            //citation: https://crypto.stackexchange.com/questions/41436/is-deriving-the-iv-from-the-password-secure
+            
+            //salt that I am using is going to be the first 16 bytes of the password (only require of a salt is to be globally unique)
+            byte[] salt = Arrays.copyOfRange(password.getBytes(), 0, 16);
+            
+            PBEKeySpec passwordPBKspec = new PBEKeySpec(password.toCharArray(), salt, 1024, 256);
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] createdPBEKS = skf.generateSecret(passwordPBKspec).getEncoded();
+
+            //IV is the next 16 bytes after key
+            byte[] IV = Arrays.copyOfRange(createdPBEKS, 16, 32);
+
+            //System.out.println(IV.length);
+
+            return IV; 
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null; 
+
+    }
 }
